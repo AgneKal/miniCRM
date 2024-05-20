b95c32c9340e26b30bb51d1cfe9da92c36081734
AgneKal <157694253+AgneKal@users.noreply.github.com>
2024-05-20 17:03:30 +0300

    animations added

--- src/app/components/contacts/new-contact/new-contact.component.ts
+++ src/app/components/contacts/new-contact/new-contact.component.ts
@@ -4,18 +4,61 @@ import { FormArray, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Va
 import { ContactService } from '../../../services/contact.service';
 import { Company } from '../../../models/company';
 import { CompaniesService } from '../../../services/companies.service';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-new-contact',
   standalone: true,
   imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './new-contact.component.html',
-  styleUrl: './new-contact.component.css'
+  styleUrl: './new-contact.component.css',
+  animations: [
+    trigger('inputHeight', [
+      state('normal', style({
+        'font-size': '22px'
+      })),
+      state('focused', style({
+        'font-size': '36px'
+      })),
+      transition('* => *', [
+        animate(300)
+      ])
+    ]),
+    trigger('phoneInput', [
+      state('*', style({
+        opacity: '1',
+        height: 'auto'
+      })),
+      transition('void=>*', [
+        style({
+          height: '0px',
+          opacity: '0',
+        }),
+        animate(500, style ({
+          height: 'auto',
+          opacity: '0',
+        })),
+        animate(750)
+      ]),
+      transition('*=>void', [
+        animate(750, style ({
+          height: 'auto',
+          transform: 'translateX(1000px)'
+        })),
+        animate(500, style ({
+          height: '0px',
+        })),
+      ])
+    ]),
+    
+  ]
 })
 
 export class NewContactComponent {
   public contactForm: FormGroup;
   public companies: Company[] = [];
+
+  public inputState = ['normal', 'normal', 'normal', 'normal', 'normal']
 
   constructor (private contactService: ContactService, private companiesService: CompaniesService){
     this.contactForm = new FormGroup ({
@@ -46,6 +89,7 @@ validatePhone(control: FormControl): ValidationErrors | null {
   onSubmit() {
     this.contactService.addContact(this.contactForm.value).subscribe(()=>{});
     this.contactForm.reset();
+    this.inputState = ['normal', 'normal', 'normal', 'normal', 'normal'];
     (this.contactForm.get('phones') as FormArray).controls = [
       new FormControl(null, Validators.required)
     ]
@@ -58,9 +102,20 @@ validatePhone(control: FormControl): ValidationErrors | null {
   public addPhoneNumber(){
     const number = new FormControl(null, [Validators.required, this.validatePhone]);
     (this.contactForm.get('phones') as FormArray).push(number);
+    this.inputState.push('normal');
   }
 
   public removePhoneNumber() {
-    (this.contactForm.get('phones') as FormArray).removeAt(-1)
+    (this.contactForm.get('phones') as FormArray).removeAt(-1);
+    this.inputState.pop();
   }
+
+  public inputFocus(fieldId:number, state:boolean){
+    if (state==true){
+      this.inputState[fieldId]='focused';
+    }else{
+      this.inputState[fieldId]='normal';
+    }
+  }
+
 }
